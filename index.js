a212db30e13a60ba46fa4ca7ef3e85db32398af9
Robert Bickley <robert.john.bickley@gmail.com>
2026-05-01 14:42:48 -0400

    Add getAverageFreelancerRate function. Fix rate variable in makeFreelancer function: PRICE_RANGE constant is an object, not an array.

--- index.js
+++ index.js
@@ -12,14 +12,25 @@ const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
 
 // === State ===
-/** @returns {Freelancer} a Freelancer profile object with name, occupation, rate. */
+/** @returns {Freelancer} a freelancer profile object with name, occupation, rate. */
 function makeFreelancer() {
   const name = NAMES[Math.floor(Math.random() * NAMES.length)];
   const occupation =
     OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
-  const rate = PRICE_RANGE[Math.floor(Math.random() * PRICE_RANCE.length)];
-
+  const rate =
+    PRICE_RANGE.min +
+    Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min));
   return { name, occupation, rate };
 }
 
 const freelancers = Array.from({ length: NUM_FREELANCERS }, makeFreelancer);
+
+/** @returns {number} the average rate of all freelancers in the state*/
+function getAverageFreelancerRate(freelancers) {
+  const totalRates = freelancers.reduce(
+    (sum, freelancer) => sum + freelancer.rate,
+    0,
+  );
+  const averageRates = totalRates / freelancers.length;
+  return averageRates;
+}
